a1b9f3dac83c0eb4ea80fc99e22ed495f758e81b
niiyazobug <niiyaz5555@gmail.com>
2022-02-28 23:58:43 +0600

    display innerHTML added

--- js/app.js
+++ js/app.js
@@ -12,21 +12,31 @@ const loadSearchPhone = () => {
         .then(res => res.json())
         .then(data => displaySearchResult(data.data))
 }
-// loadSearchPhone()
+loadSearchPhone()
 
 // display phone
 const displaySearchResult = phones => {
-    // console.log(phones)
+    console.log(phones)
     const searchResult = document.getElementById('search-result')
     // clear previous section
     searchResult.textContent = ''
-    
+
     for (const phone of phones) {
         const div = document.createElement('div')
-        div.classList.add('card')
+        div.classList.add('col')
         div.innerHTML = `
+        <div  class="card">
+        <div class="col-lg-6 mx-auto my-2"> 
         <img src="${phone.image}" class="card-img-top">
-        <h2>${phone.brand}</h2>
+        </div>
+
+        <div class="m-3 text-center">
+        <h2> Brand:${phone.brand} </h2>
+        <p>Model: ${phone.phone_name} </p>
+        <p>ID: ${phone.slug} </p>
+        <button class="bg-primary rounded text-white m-2"> Explore More Details </button>
+        </div>
+        </div>
         `
         searchResult.appendChild(div)
     }
