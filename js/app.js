19446ff3baf0bbcc8e3ced2caef005ce52274667
niiyazobug <niiyaz5555@gmail.com>
2022-02-28 23:17:21 +0600

    display search result added

--- js/app.js
+++ js/app.js
@@ -9,7 +9,26 @@ const loadSearchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
 
     fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data.data))
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data))
 }
-loadSearchPhone()
+// loadSearchPhone()
+
+// display phone
+const displaySearchResult = phones => {
+    // console.log(phones)
+    const searchResult = document.getElementById('search-result')
+    // clear previous section
+    searchResult.textContent = ''
+    
+    for (const phone of phones) {
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `
+        <img src="${phone.image}" class="card-img-top">
+        <h2>${phone.brand}</h2>
+        `
+        searchResult.appendChild(div)
+    }
+
+}
